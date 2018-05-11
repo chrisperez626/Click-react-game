e84ec9e84a00ea9b59c6bac9ae111d7c256e7ba3
chrisperez626 <chrisperez626@gmail.com>
2018-05-10 23:05:16 -0400

    intial program files

---
+++ click-react-game/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Character from './components/Character/';
+import Wrapper from "./components/Wrapper";
+import friends from './friends.json';
+
+class App extends React.Component {
+	state = {
+		friends,
+	};
+
+	render(){
+		return(
+		<Wrapper>
+		{friends.map(friend =>{
+			return (
+				
+					<Character
+						key={friend.id}
+						id={friend.id}
+						name={friend.name}
+				        image={friend.image}
+				        occupation={friend.occupation}
+				        location={friend.location}
+
+	        		/>)
+	        
+		})}
+		</Wrapper>
+		)
+	};
+}
+
+export default App;
